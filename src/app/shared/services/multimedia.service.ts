931181d0331eac42b6597d2bd8a90051fe4aa156
Axel147 <37487808+Axel147@users.noreply.github.com>
2024-02-21 17:48:12 -0300

    ya reproduce musica y muestra el tiempo

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,10 +1,69 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MultimediaService {
   callback: EventEmitter<any> = new EventEmitter<any>();
+  public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined)
+  public audio!: HTMLAudioElement
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00')
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00')
   
-  constructor() { }
+  constructor() {
+    this.audio = new Audio();
+    this.trackInfo$.subscribe(responseOk => {
+      if(responseOk){
+        this.setAudio(responseOk)
+      }
+    })
+
+    this.listenAllEvents()
+
+  }
+
+  private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false);
+
+  }
+
+  private calculateTime = () => {
+    console.log('Disparando evento')
+    const { duration, currentTime } = this.audio
+    console.table([duration, currentTime])
+    this.setTimeElapsed(currentTime)
+    this.setTimeRemainin(currentTime, duration)
+    
+  }
+
+  private setTimeElapsed(currentTime: number): void {
+    let seconds = Math.floor(currentTime % 60);
+    let minutes = Math.floor((currentTime / 60) % 60);
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+
+    const displayFormat = `${displayMinutes}:${displaySeconds}`
+    this.timeElapsed$.next(displayFormat)
+  }
+
+  private setTimeRemainin(currentTime: number, duration: number){
+    let timeLeft = duration - currentTime;
+    let seconds = Math.floor(timeLeft % 60);
+    let minutes = Math.floor((timeLeft / 60) % 60);
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(displayFormat)
+  }
+
+  public setAudio(track: TrackModel): void {
+    console.log('🎈🎈🎈',track);
+    this.audio.src = track.url
+    this.audio.play()
+  }
 }
