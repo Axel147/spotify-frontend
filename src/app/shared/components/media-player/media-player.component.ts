576316e8b2e1f9fa9f636d1a9f2203e5b8dec9b6
Axel147 <37487808+Axel147@users.noreply.github.com>
2024-02-22 13:37:10 -0300

    ya se puede poner play y pausa en la musica, tiene iconos y la barra de progreso funciona

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -9,13 +9,16 @@ import { Subscription, observable } from 'rxjs';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
-  
   listObservers$: Array<Subscription> = [];
+  state: string = 'paused'
 
   constructor(public multimediaService: MultimediaService) {}
 
   ngOnInit(): void {
-    
+    const observer1$ = this.multimediaService.playerStatus$
+    .subscribe(status => this.state = status)
+
+    this.listObservers$ = [observer1$]
   }
 
   ngOnDestroy(): void {
--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -11,6 +11,8 @@ export class MultimediaService {
   public audio!: HTMLAudioElement
   public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00')
   public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00')
+  public playerStatus$: BehaviorSubject<string> = new BehaviorSubject('paused')
+    public playerPercentage$: BehaviorSubject<number> = new BehaviorSubject(0)
   
   constructor() {
     this.audio = new Audio();
@@ -26,16 +28,44 @@ export class MultimediaService {
 
   private listenAllEvents(): void {
     this.audio.addEventListener('timeupdate', this.calculateTime, false);
+    this.audio.addEventListener('play', this.setPlayerStatus, false);
+    this.audio.addEventListener('playing', this.setPlayerStatus, false);
+    this.audio.addEventListener('pause', this.setPlayerStatus, false);
+    this.audio.addEventListener('ended', this.setPlayerStatus, false);
 
   }
 
+  private setPlayerStatus = (state: any) => {
+    switch(state.type) {
+      case 'play':
+        this.playerStatus$.next('play')
+        break
+      case 'playing':
+        this.playerStatus$.next('playing')
+        break
+      case 'ended':
+        this.playerStatus$.next('ended')
+        break
+      default:
+        this.playerStatus$.next('paused')
+        break
+    }
+  }
+
+  public togglePlayer(): void {
+    (this.audio.paused) ? this.audio.play() : this.audio.pause() 
+  }
+
   private calculateTime = () => {
-    console.log('Disparando evento')
     const { duration, currentTime } = this.audio
-    console.table([duration, currentTime])
     this.setTimeElapsed(currentTime)
     this.setTimeRemainin(currentTime, duration)
-    
+    this.setPercentage(currentTime, duration)
+  }
+
+  private setPercentage(currentTime: number, duration: number): void {
+    let percentage = (currentTime * 100) / duration;
+    this.playerPercentage$.next(percentage)
   }
 
   private setTimeElapsed(currentTime: number): void {
